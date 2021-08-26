ba401065b80049573527bd4a61d6981edf3a7664
ArnoTroch <arno.troch@student.uantwerpen.be>
2021-08-26 16:18:05 +0200

    first attempt at making a GitHub Action

--- src/config.ts
+++ src/config.ts
@@ -1,11 +1,20 @@
-import fs from "fs/promises";
+import * as core from '@actions/core';
 
 export interface IConfig {
-    url: string,    // HTTP(S) data source
-    storage: string,     // directory where data will be written
-    postprocess?: string    // path to postprocessing script, if necessary
+  url: string; // HTTP(S) data source
+  storage: string; // directory where data will be written
+  postprocess?: string; // path to postprocessing script, if necessary
 }
 
-export const getConfig = async (): Promise<IConfig> => {
-    return JSON.parse(await fs.readFile('config.json', {encoding: 'utf8'}));
+export function getConfig(): IConfig {
+  const raw: any = {};
+  const keys = ['url', 'storage', 'postprocess'];
+  keys.forEach((k) => {
+    const v = core.getInput(k); // getInput always returns a string
+    if (v) {
+      raw[k] = v;
+    }
+  });
+  core.debug(`Raw config: ${JSON.stringify(raw)}`);
+  return raw as IConfig;
 }
--- src/post.ts
+++ src/post.ts
@@ -1,39 +1,47 @@
 // SOURCE: https://github.com/githubocto/flat/blob/main/src/post.ts
-import * as core from '@actions/core'
-import {exec} from '@actions/exec'
+import * as core from '@actions/core';
+import { exec } from '@actions/exec';
 
-export const runPostScript = async () => {
-    core.startGroup('Post cleanup script')
+const run = async () => {
+  core.startGroup('Post cleanup script');
 
-    if (process.env.HAS_RUN_POST_JOB) {
-        core.info('Files already committed');
-        core.endGroup();
-        return;
-    }
-
-    const files = JSON.parse(process.env.FILES || '[]');
-    core.info('files');
-    core.info(JSON.stringify(files));
-
-    // Don't want to commit if there aren't any files changed!
-    if (!files.length) {
-        core.info('No changes to commit');
-        core.endGroup();
-        return;
-    }
-
-    const date = new Date().toISOString();
-    const meta = JSON.stringify({date, files}, undefined, 2);
-    const msg = `Flat: latest data (${date})`;
-    const body = files.map((f: { [x: string]: any; }) => f['name']).join('\n- ');
-
-    // these should already be staged, in main.ts
-    core.info(`Committing "${msg}"`);
-    core.debug(meta);
-    await exec('git', ['commit', '-m', msg + '\nCreated/changed files:\n- ' + body]);
-    await exec('git', ['push']);
-    core.info(`Pushed!`);
-    core.exportVariable('HAS_RUN_POST_JOB', 'true');
-
+  if (process.env.HAS_RUN_POST_JOB) {
+    core.info('Files already committed');
     core.endGroup();
-}
+    return;
+  }
+
+  const files = JSON.parse(process.env.FILES || '[]');
+  core.info('files');
+  core.info(JSON.stringify(files));
+
+  // Don't want to commit if there aren't any files changed!
+  if (!files.length) {
+    core.info('No changes to commit');
+    core.endGroup();
+    return;
+  }
+
+  const date = new Date().toISOString();
+  const meta = JSON.stringify({ date, files }, undefined, 2);
+  const msg = `Flat: latest data (${date})`;
+  const body = files.map((f: { [x: string]: any }) => f['name']).join('\n- ');
+
+  // these should already be staged, in main.ts
+  core.info(`Committing "${msg}"`);
+  core.debug(meta);
+  await exec('git', [
+    'commit',
+    '-m',
+    msg + '\nCreated/changed files:\n- ' + body,
+  ]);
+  await exec('git', ['push']);
+  core.info(`Pushed!`);
+  core.exportVariable('HAS_RUN_POST_JOB', 'true');
+
+  core.endGroup();
+};
+
+run().catch((error) => {
+  core.setFailed('Post script failed! ' + error.message);
+});
