af78f306002b99a8ffb0de75030cd911045fcb82
ArnoTroch <arno.troch@student.uantwerpen.be>
2021-08-30 09:25:24 +0200

    attempt at github action

--- src/data.ts
+++ src/data.ts
@@ -1,80 +1,90 @@
-import {newEngine} from '@treecg/actor-init-ldes-client';
+import { newEngine } from '@treecg/actor-init-ldes-client';
 import * as fs from 'fs/promises';
-import {existsSync, mkdirSync} from 'fs';
-import {IConfig} from "./config";
-
+import { existsSync, mkdirSync } from 'fs';
+import { IConfig } from './config';
 
 export class Data {
-    public readonly config: IConfig;
-    public readonly members: { [key: string]: object };
+	public readonly config: IConfig;
+	public readonly members: { [key: string]: object };
 
-    public constructor(config: IConfig) {
-        this.config = config;
-        this.members = {};
-    }
+	public constructor(config: IConfig) {
+		this.config = config;
+		this.members = {};
+	}
 
-    /**
-     * fetch data using the LDES client and store them in 'members'
-     */
-    public async fetchData(): Promise<void> {
-        return new Promise<void>((resolve, reject) => {
-            try {
-                let options = {
-                    "pollingInterval": 5000, // milliseconds
-                    "emitMemberOnce": true,
-                    "disablePolling": true,
-                };
+	/**
+	 * fetch data using the LDES client and store them in 'members'
+	 */
+	public async fetchData(): Promise<void> {
+		return new Promise<void>((resolve, reject) => {
+			try {
+				let options = {
+					pollingInterval: 5000, // milliseconds
+					emitMemberOnce: true,
+					disablePolling: true,
+				};
 
-                let LDESClient = newEngine();
-                let eventStreamSync = LDESClient.createReadStream(this.config.url, options);
-                eventStreamSync
-                    .on('data', (data) => {
-                        let obj = JSON.parse(data);
-                        this.members[obj['@id']] = obj;
-                    })
-                    .on('end', async () => {
-                        return resolve();
-                    });
+				let LDESClient = newEngine();
+				let eventStreamSync = LDESClient.createReadStream(
+					this.config.url,
+					options
+				);
+				eventStreamSync
+					.on('data', (data) => {
+						let obj = JSON.parse(data);
+						this.members[obj['@id']] = obj;
+					})
+					.on('end', async () => {
+						return resolve();
+					});
+			} catch (e) {
+				console.error(e);
+				return reject(e);
+			}
+		});
+	}
 
-            } catch (e) {
-                console.error(e);
-                return reject(e);
-            }
-        });
-    }
+	/**
+	 * write data stored in 'members' to the output directory supplied in the config file
+	 */
+	public async writeData(): Promise<void> {
+		return new Promise<void>(async (resolve, reject) => {
+			try {
+				const now = new Date().toISOString();
+				// create necessary directories where data will be stored
+				if (!existsSync(this.config.storage)) {
+					mkdirSync(this.config.storage);
+				}
+				mkdirSync(`${this.config.storage}/${now}`);
 
-    /**
-     * write data stored in 'members' to the output directory supplied in the config file
-     */
-    public async writeData(): Promise<void> {
-        return new Promise<void>(async (resolve, reject) => {
-            try {
-                const now = new Date().toISOString();
-                // create necessary directories where data will be stored
-                if (!existsSync(this.config.storage)) {
-                    mkdirSync(this.config.storage);
-                }
-                mkdirSync(`${this.config.storage}/${now}`);
+				// split members into multiple files, each containing no more than 500 members
+				const file_size = 500;
+				const member_values = Object.values(this.members);
+				const chunks = Array.from(
+					new Array(Math.ceil(member_values.length / file_size)),
+					(_, i) =>
+						member_values.slice(
+							i * file_size,
+							i * file_size + file_size
+						)
+				);
+				// write all chunks to a file
+				await Promise.all(
+					chunks.map((chunk, index) => {
+						// files are named data<number>.json, where <number> is a 5-digit number representing the chunk index
+						const file_num = String(index).padStart(5, '0');
+						fs.writeFile(
+							`${this.config.storage}/${now}/data${file_num}.json`,
+							JSON.stringify(chunk)
+						);
+					})
+				);
 
-                // split members into multiple files, each containing no more than 500 members
-                const file_size = 500;
-                const member_values = Object.values(this.members);
-                const chunks = Array.from(
-                    new Array(Math.ceil(member_values.length / file_size)), (_, i) =>
-                        member_values.slice(i * file_size, i * file_size + file_size)
-                );
-                // write all chunks to a file
-                await Promise.all(chunks.map((chunk, index) => {
-                    // files are named data<number>.json, where <number> is a 5-digit number representing the chunk index
-                    const file_num = String(index).padStart(5, '0');
-                    fs.writeFile(`${this.config.storage}/${now}/data${file_num}.json`, JSON.stringify(chunk));
-                }));
-
-                return resolve();
-            } catch (e) {
-                console.error(e);
-                return reject(e);
-            }
-        });
-    }
+				return resolve();
+			} catch (e) {
+				console.error(e);
+				return reject(e);
+			}
+		});
+	}
 }
--- src/main.ts
+++ src/main.ts
@@ -1,87 +1,87 @@
 // SOURCE: https://github.com/githubocto/flat/blob/main/src/main.ts
-import * as core from '@actions/core'
-import {exec} from '@actions/exec'
-import {execSync} from 'child_process'
-import {diff} from './git'
-import {IConfig} from "./config";
-import {Data} from "./data";
-import {runPostScript} from "./post";
-// @ts-ignore
-import * as rawConfig from '../config.json';
+import * as core from '@actions/core';
+import { exec } from '@actions/exec';
+import { execSync } from 'child_process';
+import { diff } from './git';
+import { getConfig, IConfig } from './config';
+import { Data } from './data';
 
+const run = async (): Promise<void> => {
+	core.startGroup('Configuration');
+	const config: IConfig = getConfig();
+	core.info(config.url + ' - ' + config.storage);
+	const username = 'flat-data';
+	await exec('git', ['config', 'user.name', username]);
+	await exec('git', [
+		'config',
+		'user.email',
+		`${username}@users.noreply.github.com`,
+	]);
+	core.endGroup();
 
-async function run(): Promise<void> {
-    core.startGroup('Configuration');
-    const config: IConfig = rawConfig;
-    const username = 'flat-data';
-    await exec('git', ['config', 'user.name', username]);
-    await exec('git', [
-        'config',
-        'user.email',
-        `${username}@users.noreply.github.com`,
-    ]);
-    core.endGroup();
+	core.startGroup('Fetch and write data');
+	const data_fetcher = new Data(config);
+	await data_fetcher.fetchData();
+	await data_fetcher.writeData();
+	core.endGroup();
 
-    core.startGroup('Fetch and write data');
-    const data_fetcher = new Data(config);
-    await data_fetcher.fetchData();
-    await data_fetcher.writeData();
-    core.endGroup();
+	if (config.postprocess) {
+		core.startGroup('Postprocess');
+		core.debug(`Invoking ${config.postprocess}…`);
+		try {
+			core.info("TODO (doesn't run anything yet)");
+		} catch (error) {
+			// @ts-ignore
+			core.setFailed(error);
+		}
+		core.endGroup();
+	}
 
-    if (config.postprocess) {
-        core.startGroup('Postprocess');
-        core.debug(`Invoking ${config.postprocess}…`)
-        try {
-            core.info('TODO (doesn\'t run anything yet)')
-        } catch (error) {
-            core.setFailed(error);
-        }
-        core.endGroup()
-    }
+	core.startGroup('File changes');
+	const newUnstagedFiles = execSync(
+		'git ls-files --others --exclude-standard'
+	).toString();
+	const modifiedUnstagedFiles = execSync('git ls-files -m').toString();
+	const editedFilenames = [
+		...newUnstagedFiles.split('\n'),
+		...modifiedUnstagedFiles.split('\n'),
+	].filter(Boolean);
 
-    core.startGroup('File changes');
-    const newUnstagedFiles = execSync('git ls-files --others --exclude-standard').toString();
-    const modifiedUnstagedFiles = execSync('git ls-files -m').toString();
-    const editedFilenames = [
-        ...newUnstagedFiles.split('\n'),
-        ...modifiedUnstagedFiles.split('\n'),
-    ].filter(Boolean);
+	core.info('newUnstagedFiles');
+	core.info(newUnstagedFiles + '');
+	core.info('modifiedUnstagedFiles');
+	core.info(modifiedUnstagedFiles + '');
+	core.info('editedFilenames');
+	core.info(JSON.stringify(editedFilenames));
+	core.endGroup();
 
-    core.info('newUnstagedFiles');
-    core.info(newUnstagedFiles + '');
-    core.info('modifiedUnstagedFiles');
-    core.info(modifiedUnstagedFiles + '');
-    core.info('editedFilenames');
-    core.info(JSON.stringify(editedFilenames));
-    core.endGroup();
+	core.startGroup('Calculating diffstat');
+	const editedFiles = [];
+	for (const filename of editedFilenames) {
+		core.debug(`git adding ${filename}…`);
+		await exec('git', ['add', filename]);
+		const bytes = await diff(filename);
+		editedFiles.push({
+			name: filename,
+			deltaBytes: bytes,
+			source: config.url,
+		});
+	}
+	core.endGroup();
 
-    core.startGroup('Calculating diffstat');
-    const editedFiles = [];
-    for (const filename of editedFilenames) {
-        core.debug(`git adding ${filename}…`);
-        await exec('git', ['add', filename]);
-        const bytes = await diff(filename);
-        editedFiles.push({'name': filename, 'deltaBytes': bytes, 'source': config.url});
-    }
-    core.endGroup();
+	core.startGroup('Committing new data');
+	const alreadyEditedFiles = JSON.parse(process.env.FILES || '[]');
+	core.info('alreadyEditedFiles');
+	core.info(JSON.stringify(alreadyEditedFiles));
+	core.info('editedFiles');
+	core.info(JSON.stringify(editedFiles));
+	const files = [...alreadyEditedFiles, ...editedFiles];
+	core.exportVariable('FILES', files);
+	core.info('process.env.FILES');
+	core.info(JSON.stringify(process.env.FILES));
+	core.endGroup();
+};
 
-    core.startGroup('Committing new data');
-    const alreadyEditedFiles = JSON.parse(process.env.FILES || '[]');
-    core.info('alreadyEditedFiles');
-    core.info(JSON.stringify(alreadyEditedFiles));
-    core.info('editedFiles');
-    core.info(JSON.stringify(editedFiles));
-    const files = [...alreadyEditedFiles, ...editedFiles];
-    core.exportVariable('FILES', files);
-    core.info('process.env.FILES');
-    core.info(JSON.stringify(process.env.FILES));
-    core.endGroup();
-}
-
-run()
-    .then(async () => {
-        await runPostScript();
-    })
-    .catch(error => {
-        core.setFailed('Workflow failed! ' + error.message);
-    });
+run().catch((error) => {
+	core.setFailed('Workflow failed! ' + error.message);
+});
