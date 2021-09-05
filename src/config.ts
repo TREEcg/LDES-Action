c0f1a35b6e2ec3d1f293ee5c11bc4e5b325b36ae
Kasper Zutterman <kasperzutterman1999@gmail.com>
2021-09-05 19:11:40 +0200

    feat: update to ignore output

--- src/config.ts
+++ src/config.ts
@@ -6,11 +6,13 @@ export interface IConfig {
 	postprocess?: string; // path to postprocessing script, if necessary
 	gh_pages_branch: string; // branch used for deploying to GitHub Pages
 	gh_pages_url: string; // URL of the GitHub Pages deployment
+	git_username: string; // GitHub username that makes the commits
+	git_email: string; // GitHub email that makes the commits
 }
 
 export function getConfig(): IConfig {
 	const raw: any = {};
-	const keys = ['url', 'storage', 'postprocess', 'gh_pages_branch', 'gh_pages_url'];
+	const keys = ['url', 'storage', 'postprocess', 'gh_pages_branch', 'gh_pages_url', 'git_username', 'git_email'];
 	keys.forEach((k) => {
 		const v = core.getInput(k); // getInput always returns a string
 		core.info(`${k}: ${v}`);
