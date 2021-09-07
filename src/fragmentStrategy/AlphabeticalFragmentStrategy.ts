c4b455e5506cef7ef089eed53d46497c6cde9beb
Kasper Zutterman <kasperzutterman1999@gmail.com>
2021-09-07 13:13:00 +0200

    feat: add tree hypermedia

--- src/fragmentStrategy/AlphabeticalFragmentStrategy.ts
+++ src/fragmentStrategy/AlphabeticalFragmentStrategy.ts
@@ -1,7 +1,7 @@
 import IFragmentStrategy from './IFragmentStrategy';
 import type * as RDF from 'rdf-js';
+import { literal, namedNode, blankNode, quad } from '@rdfjs/data-model';
 import fs from 'fs';
-import date from "../utils/date";
 import { IConfig } from '../config';
 const N3 = require('n3');
 
@@ -26,43 +26,24 @@ class AlphabeticalFragmentStrategy implements IFragmentStrategy {
             pages[index] = pages[index].concat(sortedData[i]);
         }
 
-        pages.forEach((page, index) => { 
+        pages.forEach((page, index) => {
+            let fileLocation = this.getFileLocation(index, config);
+            // next page
+            let nextPage = (index + 1 < pages.length) ? this.getFileLocation(index + 1, config) : null;
+            // previous page
+            let previousPage = (index > 0) ? this.getFileLocation(index - 1, config) : null;
+
+            // add hypermedia
+            page = page.concat(this.createHypermedia(page, config, fileLocation, nextPage, previousPage));
+
             // make file where we will store newly fetched data     
-            //const writer = new N3.Writer({ format: 'N-Triples' });
             const writer = new N3.Writer({ format: 'Turtle' });
             let serialised = writer.quadsToString(page);
 
-            const fileName = String(index).padStart(5, '0');
-            fs.writeFileSync(`${config.storage}/${fileName}.ttl`, serialised);
+            fs.writeFileSync(fileLocation, serialised);
         });
-        /*
-        data.forEach(quadArr => {
-            let identifier = this.find(quadArr, 'http://purl.org/dc/terms/isVersionOf');
-            let reference = identifier.substring(identifier.lastIndexOf('/') + 1);
-
-            let generatedAtTime = this.find(quadArr, 'http://www.w3.org/ns/prov#generatedAtTime');
-            let basicISODate = date.dateToBasicISODate(new Date(generatedAtTime));
-
-            // check if directory does not exist
-            if (!fs.existsSync(`${config.storage}/${reference}`)) {
-                console.log('Directory not existing!');
-                // make directory where we will store newly fetched data
-                fs.mkdirSync(`${config.storage}/${reference}`);
-            }
-
-            // check if file not exists
-            if (!fs.existsSync(`${config.storage}/${reference}/${basicISODate}.nt`)) {
-                // make file where we will store newly fetched data     
-                const writer = new N3.Writer({ format: 'N-Triples' });
-                let serialised = writer.quadsToString(quadArr);
-
-                fs.writeFileSync(`${config.storage}/${reference}/${basicISODate}.nt`, serialised);
-            }
-
-        });
-        */
     }
-
+  
     sort(data: RDF.Quad[][], predicate: string): RDF.Quad[][] {
         return data.sort((a, b) => {
             let identifierA = this.find(a, predicate);
@@ -74,6 +55,41 @@ class AlphabeticalFragmentStrategy implements IFragmentStrategy {
     find(data: RDF.Quad[], predicate: string): any {
         const found = data.find(element => element.predicate.value === predicate);
         return (found === undefined) ? null : found.object.value;
+    }
+
+    findAllMembers(data: RDF.Quad[]): string[] { 
+        return [... new Set(data.map(element => element.subject.value))];
+    }
+
+    getFileLocation(index: number, config: IConfig): string {
+        // file location
+        const fileName = String(index).padStart(5, '0');
+        return `${config.storage}/${fileName}.ttl`;
+    }
+
+    createHypermedia(data: RDF.Quad[], config: IConfig, fileLocation: string, nextPage: string | null, previousPage: string | null): RDF.Quad[] {
+        let hypermedia: RDF.Quad[] = [];
+        // add tree:member
+        let treeMembers: string[] = this.findAllMembers(data);
+        treeMembers.forEach(member => {
+            hypermedia.push(quad(namedNode("https://smartdata.dev-vlaanderen.be/base/gemeente"), namedNode("https://w3id.org/tree#member"), namedNode(member)));
+        });
+        //  add tree:view
+        hypermedia.push(quad(namedNode("https://smartdata.dev-vlaanderen.be/base/gemeente"), namedNode("https://w3id.org/tree#view"), namedNode(`${config.gh_pages_url}/${fileLocation}`)));
+
+        // add tree:relation
+        if (nextPage) {
+            hypermedia.push(quad(namedNode(`${config.gh_pages_url}/${fileLocation}`), namedNode("https://w3id.org/tree#relation"), blankNode("Relation_Next")));
+            hypermedia.push(quad(blankNode("Relation_Next"), namedNode("http://www.w3.org/1999/02/22-rdf-syntax-ns#type"), namedNode("https://w3id.org/tree#Relation")));
+            hypermedia.push(quad(blankNode("Relation_Next"), namedNode("https://w3id.org/tree#node"), namedNode(`${config.gh_pages_url}/${nextPage}`)));
+        }
+        if (previousPage) {
+            hypermedia.push(quad(namedNode(`${config.gh_pages_url}/${fileLocation}`), namedNode("https://w3id.org/tree#relation"), blankNode("Relation_Previous")));
+            hypermedia.push(quad(blankNode("Relation_Previous"), namedNode("http://www.w3.org/1999/02/22-rdf-syntax-ns#type"), namedNode("https://w3id.org/tree#Relation")));
+            hypermedia.push(quad(blankNode("Relation_Previous"), namedNode("https://w3id.org/tree#node"), namedNode(`${config.gh_pages_url}/${previousPage}`)));
+        }
+
+        return hypermedia;
     }
 
 }
