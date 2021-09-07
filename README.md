06c124cb0e1e885cf872d4ec180378777b960c15
Kasper Zutterman <kasperzutterman1999@gmail.com>
2021-09-07 15:20:15 +0200

    Feat cleanup (#8)
    * fix: delete commented code
    
    * fix: remove unused env variable
    
    * fix: remove dummy data
    
    * fix: remove unused variables
    
    * fix: remove unused fetchTime code
    
    * feat: update readme with new env variables

--- README.md
+++ README.md
@@ -56,14 +56,20 @@ URL to a LDES or tree:Collection dataset from which you want to fetch data.
 
 Name of the output directory where the fetched data will be stored.
 
-### `postprocess` (optional)
+### `gh_pages_url` (optional)
 
-Path to a JavaScript file for postprocessing the data in `storage`. *(not implemented yet)*
+URL where GitHub Pages will be deployed.
+Default: `http(s)://<username>.github.io/<repository> or http(s)://<organization>.github.io/<repository>`
 
-### `gh_pages_branch` (optional)
+### `fragmentation_strategy` (optional)
 
-Branch where GitHub Pages will be deployed. The branch will be created if it doesn't exist yet.\
-Default: `'gh-pages'`
+Fragmentation strategy that will be deployed.
+Default: `version`
+
+### `fragmentation_page_size` (optional)
+
+Amount of RDF objects that will be on a single page.
+Default: `'50'`
 
 ## Outputs
 
--- src/data.ts
+++ src/data.ts
@@ -7,25 +7,16 @@ import * as N3 from 'n3';
 import date from "./utils/date";
 import type * as RDF from 'rdf-js';
 import { literal, namedNode, blankNode, quad } from '@rdfjs/data-model';
-import { getDummyData } from './dummyData';
 import FragmentContext from './fragmentStrategy/FragmentContext';
 import VersionFragmentStrategy from './fragmentStrategy/VersionFragmentStrategy';
 import IFragmentStrategy from './fragmentStrategy/IFragmentStrategy';
 import AlphabeticalFragmentStrategy from './fragmentStrategy/AlphabeticalFragmentStrategy';
 
 export class Data {
-	// name of files where data will be stored
-	private readonly DATA_FILE = 'data';
-	// amount of objects we save per file;
-	private readonly FILE_SIZE = 500;
 
 	private readonly config: IConfig;
 	private readonly store: N3.Store;
-	private readonly fetches: Array<string>;
-	private fetch_time: string | undefined;
 	private fragmentContext: FragmentContext;
-
-	private dummyData: RDF.Quad[][] = [];
 
 	public constructor(config: IConfig) {
 		this.config = config;
@@ -36,20 +27,9 @@ export class Data {
 			mkdirSync(this.config.storage);
 		}
 
-		// load previous fetch times if they exist. We will use the most recent fetch time
-		// to only fetch data that was added after this time.
-		const fetch_times = fs.readdirSync(this.config.storage);
-		const fetch_dates = fetch_times
-			.map((dir) => new Date(dir))
-			// @ts-ignore
-			.filter((date) => !isNaN(date));
-		this.fetches = fetch_dates.map((date) => date.toISOString());
-		this.fetches.sort();
-
 		this.fragmentContext = new FragmentContext(new VersionFragmentStrategy());
 		this.setFragmentationStrategy();
 
-		this.dummyData = getDummyData();
 	}
 
 	/**
@@ -86,9 +66,6 @@ export class Data {
 					emitMemberOnce: true,
 					disablePolling: true,
 					mimeType: 'text/turtle',
-					// only fetch data added after latest fetch
-					fromTime:
-						this.fetches.length > 0 ? new Date(this.fetches[0]) : undefined,
 				};
 
 				let LDESClient = newEngine();
@@ -114,7 +91,6 @@ export class Data {
 					if (quad) {
 						this.store.addQuad(quad);
 					} else {
-						this.fetch_time = new Date().toISOString();
 						return resolve();
 					}
 				});
@@ -131,57 +107,12 @@ export class Data {
 	public async writeData(): Promise<void> {
 		return new Promise<void>(async (resolve, reject) => {
 			try {
-				/*
-				if (this.store.countQuads(null, null, null, null) === 0) {
-					// if there is no data, we are done
-					return resolve();
-				}
-
-				// make directory where we will store newly fetched data
-				let basicISODate = date.dateToBasicISODate(new Date());
-				console.log(`${this.config.storage}/${basicISODate}`)
-				mkdirSync(`${this.config.storage}/${basicISODate}`);
-
-
-				// split quads into multiple chunks containing 'this.FILE_SIZE' different subjects
-				const subjects = this.store.getSubjects(null, null, null);
-				const chunks = Array.from(
-					new Array(Math.ceil(subjects.length / this.FILE_SIZE)),
-					(_, i) =>
-						subjects.slice(
-							i * this.FILE_SIZE,
-							i * this.FILE_SIZE + this.FILE_SIZE
-						)
-				);
-
-				// write each chunk to its own file
-				await Promise.all(
-					chunks.map((chunk, index) => {
-						let writer = new N3.Writer();
-						chunk.forEach((subject) =>
-							writer.addQuads(this.store.getQuads(subject, null, null, null))
-						);
-						writer.end((err, result) => {
-							if (err) {
-								return reject(err);
-							}
-							// files are named <this.DATA_FILE><number>.ttl, where <number> is a 5-digit number
-							// representing the chunk index
-							const file_num = String(index).padStart(5, '0');
-							fs.promises.writeFile(
-								`${this.config.storage}/${basicISODate}/${this.DATA_FILE}${file_num}.ttl`,
-								result
-							);
-						});
-					})
-				);
-
-				return resolve();
-				*/
+				// reshape data to RDF.Quad[][] format
 				let respapedData: RDF.Quad[][] = this.reshapeData();
+
+				// fragment data & write to files
 				this.fragmentContext.fragment(respapedData, this.config);
 
-				//this.fragmentContext.fragment(this.dummyData, this.config);
 				return resolve();
 			} catch (e) {
 				console.error(e);
