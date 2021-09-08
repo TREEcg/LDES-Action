187005e593fcb4b5632543c084bc561ee91bcd25
Kasper Zutterman <kasperzutterman1999@gmail.com>
2021-09-08 11:59:16 +0200

    Feat symbolic link (#10)
    * fix: change type to correct interface
    
    * feat: add symbolic link to newest version
    
    * feat: add symbolic link to newest version
    
    * feat: add symbolic link to newest version
    
    * fix: remove old symbolic link before creating new one
    
    * fix: remove old symbolic link before creating new one
    
    * fix: add / before symlink destination
    
    * fix: change symlink destination

--- src/fragmentStrategy/VersionFragmentStrategy.ts
+++ src/fragmentStrategy/VersionFragmentStrategy.ts
@@ -2,6 +2,7 @@ import IFragmentStrategy from './IFragmentStrategy';
 import type * as RDF from 'rdf-js';
 import fs from 'fs';
 import date from "../utils/date";
+import { IConfig } from '../config';
 const N3 = require('n3');
 
 /**
@@ -10,7 +11,7 @@ const N3 = require('n3');
  */
 class VersionFragmentStrategy implements IFragmentStrategy {
 
-    fragment(data: RDF.Quad[][], config: { storage: any }): void {
+    fragment(data: RDF.Quad[][], config: IConfig): void {
         data.forEach(quadArr => {
             let identifier = this.find(quadArr, 'http://purl.org/dc/terms/isVersionOf');
             let reference = identifier.substring(identifier.lastIndexOf('/') + 1);
@@ -35,11 +36,41 @@ class VersionFragmentStrategy implements IFragmentStrategy {
             }
 
         });
+
+        this.addSymbolicLinks(config);
     }
 
     find(data: RDF.Quad[], predicate: string): any {
         const found = data.find(element => element.predicate.value === predicate);
         return (found === undefined) ? null : found.object.value;
+    }
+
+    addSymbolicLinks(config: IConfig): void {
+        // get all directories in the storage directory
+        const directories = fs.readdirSync(config.storage).filter(
+            (file: string) => fs.statSync(`${config.storage}/${file}`).isDirectory()
+        );
+
+        // get all filenames in the current directory
+        directories.forEach(directory => {
+            const files: string[] = fs.readdirSync(`${config.storage}/${directory}`);
+
+            // sort files array lexicographically to get the latest version
+            let latest = files.sort()[files.length - 1];
+
+            // create latest.ttl file
+            // fs.writeFileSync(`${config.storage}/${directory}/latest.ttl`, '');
+            // check if symbolic link already exists
+            if (fs.existsSync(`${config.storage}/${directory}/latest.ttl`)) {
+                // delete symbolic link
+                fs.unlinkSync(`${config.storage}/${directory}/latest.ttl`);
+                console.log(`unlinked ${config.storage}/${directory}/latest.ttl`);
+            }
+
+            // create symbolic link latets.ttl -> latest file
+            //fs.symlinkSync(`/${config.storage}/${directory}/${latest}`, `${config.storage}/${directory}/latest.ttl`);
+            fs.symlinkSync(`${latest}`, `${config.storage}/${directory}/latest.ttl`);
+        });
     }
 
 }
