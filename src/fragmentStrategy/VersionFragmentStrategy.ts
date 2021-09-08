3d7f46ccab69daf3658eaa54dcc764afefeb3a92
Kasper Zutterman <kasperzutterman1999@gmail.com>
2021-09-08 11:33:22 +0200

    fix: add / before symlink destination

--- src/fragmentStrategy/VersionFragmentStrategy.ts
+++ src/fragmentStrategy/VersionFragmentStrategy.ts
@@ -68,7 +68,7 @@ class VersionFragmentStrategy implements IFragmentStrategy {
             }
 
             // create symbolic link latets.ttl -> latest file
-            fs.symlinkSync(`${config.storage}/${directory}/${latest}`, `${config.storage}/${directory}/latest.ttl`);
+            fs.symlinkSync(`/${config.storage}/${directory}/${latest}`, `${config.storage}/${directory}/latest.ttl`);
         });
     }
 
