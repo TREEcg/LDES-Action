73791018120883b8b85bc6d759f1f65745c7aa41
Kasper Zutterman <kasperzutterman1999@gmail.com>
2021-09-09 10:25:21 +0200

    feat: remove output dir before running with new data

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,18 @@ import { execSync } from 'child_process';
 import { diff } from './git';
 import { getConfig, IConfig } from './config';
 import { Data } from './data';
+import { rmdirSync } from 'fs';
 
 const run = async (): Promise<void> => {
 	core.startGroup('Configuration');
 	const config: IConfig = getConfig();
 	await exec('git', ['config', 'user.name', config.git_username]);
 	await exec('git', ['config', 'user.email', `${config.git_email}`]);
+	core.endGroup();
+
+	core.startGroup('Delete output folder');
+	// Delete output folder to have a clean start, no symlinks, no old data
+	rmdirSync(config.storage, { recursive: true });
 	core.endGroup();
 
 	core.startGroup('Fetch and write data');
