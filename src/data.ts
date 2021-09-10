677ea4b3811ce2810168d5ff84d9eac736ab201f
Kasper Zutterman <kasperzutterman1999@gmail.com>
2021-09-10 16:06:43 +0200

    fix: remove unused imports and commented code

--- src/data.ts
+++ src/data.ts
@@ -1,12 +1,7 @@
-import { newEngine } from '@treecg/actor-init-ldes-client';
-import fs from 'fs';
 import { existsSync, mkdirSync } from 'fs';
 import { IConfig } from './config';
-import * as N3 from 'n3';
 
-import date from "./utils/date";
 import type * as RDF from 'rdf-js';
-import { literal, namedNode, blankNode, quad } from '@rdfjs/data-model';
 import FragmentContext from './fragmentStrategy/FragmentContext';
 import SubjectPagesFragmentStrategy from './fragmentStrategy/SubjectPagesFragmentStrategy';
 import IFragmentStrategy from './fragmentStrategy/IFragmentStrategy';
@@ -84,52 +79,6 @@ export class Data {
 	}
 
 	/**
-	 * fetch data using the LDES client
-	 
-	public async fetchData(): Promise<void> {
-		return new Promise<void>((resolve, reject) => {
-			try {
-				let options = {
-					emitMemberOnce: true,
-					disablePolling: true,
-					mimeType: 'text/turtle',
-				};
-
-				let LDESClient = newEngine();
-				let eventStreamSync = LDESClient.createReadStream(
-					this.config.url,
-					options
-				);
-
-				// @ Here should come the RDF.Quad[][] implementation when it is finished in the library!
-				// It should replace the current N3 Parser implementation.
-
-				const parser = new N3.Parser({ format: 'text/turtle' });
-
-				// read quads and add them to triple store
-				// @ts-ignore
-				parser.parse(eventStreamSync, (err, quad, prefs) => {
-					if (err) {
-						return reject(err);
-					}
-					if (prefs) {
-						console.log('prefixes:', prefs);
-					}
-					if (quad) {
-						this.store.addQuad(quad);
-					} else {
-						return resolve();
-					}
-				});
-			} catch (e) {
-				console.error(e);
-				return reject(e);
-			}
-		});
-	}
-	*/
-
-	/**
 	 * fetch data using Datasource
 	 */
 	public async fetchData():  Promise<void> {
@@ -161,38 +110,4 @@ export class Data {
 		});
 	}
 
-	/*
-	// This code should be deprecatable when issue https://github.com/TREEcg/event-stream-client/issues/22 is fixed
-	private reshapeData(): RDF.Quad[][] {
-		let reshapedData: RDF.Quad[][] = [];
-
-		let uniqueSubjects = this.store.getSubjects(null, null, null);
-		console.log(uniqueSubjects[0].value);
-
-		uniqueSubjects.forEach((subject) => {
-			let subjectQuads = this.store.getQuads(subject, null, null, null);
-
-			// N3.Quad to RDF.Quad
-			
-			let reshapedSubjectQuads: RDF.Quad[] = [];
-			subjectQuads.forEach((_quad) => {
-				let quadSubject = _quad.subject;
-				let quadPredicate = _quad.predicate;
-				let quadObject = _quad.object;
-				let quadGraph = _quad.graph;
-
-				let reshapedQuad = quad(quadSubject, quadPredicate,	quadObject,	quadGraph);
-
-				reshapedSubjectQuads.push(reshapedQuad);
-			});
-
-			reshapedData.push(reshapedSubjectQuads);
-			
-			//console.log(subjectQuads);
-			//console.log(reshapedSubjectQuads);
-		});
-
-		return reshapedData;
-	}
-	*/
 }
