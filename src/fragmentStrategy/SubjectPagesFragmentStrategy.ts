18d6b177b62b66b7a3bef814849118c1258eae44
lucas derveaux <lucasderveaux1@gmail.com>
2021-09-15 09:04:06 +0200

    subject-pages terug ingeschakeld

--- src/fragmentStrategy/SubjectPagesFragmentStrategy.ts
+++ src/fragmentStrategy/SubjectPagesFragmentStrategy.ts
@@ -13,20 +13,22 @@ const N3 = require('n3');
 class SubjectPagesFragmentStrategy implements IFragmentStrategy {
 
     fragment(data: IData[], config: IConfig): void {
-        let cijfer:number = 0;
+        let cijfer: number = 0;
 
         data.forEach((_data: IData) => {
-            // let identifier;
-            // let reference;
-            // if (config.version_materialize) {
-            //     identifier = this.find(_data.quads, 'http://purl.org/dc/terms/hasVersion', true);
-            //     reference = identifier.substring(identifier.lastIndexOf('/') + 1);
-            // } else {
-                // let identifier = this.find(_data.quads, 'http://purl.org.dc/terms/isVersionOf', false);
-                // let reference = identifier.substring(identifier.lastIndexOf('/') + 1);
-            // }
+            let identifier;
             let reference = cijfer;
-            cijfer++;
+            try {
+                if (config.version_materialize=='true') {
+                    identifier = this.find(_data.quads, 'http://purl.org/dc/terms/hasVersion', true);
+                    reference = identifier.substring(identifier.lastIndexOf('/') + 1);
+                } else {
+                    identifier = this.find(_data.quads, 'http://purl.org.dc/terms/isVersionOf', false);
+                    reference = identifier.substring(identifier.lastIndexOf('/') + 1);
+                }
+            } catch (e) {
+                cijfer++;
+            }
 
             let generatedAtTime = this.find(_data.quads, 'http://www.w3.org/ns/prov#generatedAtTime', false);
             let basicISODate = date.dateToBasicISODate(new Date(generatedAtTime));
