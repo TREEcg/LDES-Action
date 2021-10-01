07cc751cbbde570d877e3f44c7cc9424cf29c8e5
Kasper Zutterman <kasperzutterman1999@gmail.com>
2021-10-01 16:02:05 +0200

    lint: lint

--- src/datasourceStrategy/LDESClientDatasource.ts
+++ src/datasourceStrategy/LDESClientDatasource.ts
@@ -6,48 +6,43 @@ import { newEngine } from '@treecg/actor-init-ldes-client';
 import IMember from '../IMember';
 
 class LDESClientDatasource implements IDatasource {
+	async getData(config: IConfig): Promise<IMember[]> {
+		return new Promise<IMember[]>((resolve, reject) => {
+			try {
+				let options = {
+					emitMemberOnce: true,
+					disablePolling: true,
+					mimeType: 'text/turtle',
+					representation: 'Quads',
+				};
 
-    async getData(config: IConfig): Promise<IMember[]> {
-        return new Promise<IMember[]>((resolve, reject) => {
-            try {
-                let options = {
-                    emitMemberOnce: true,
-                    disablePolling: true,
-                    mimeType: 'text/turtle',
-                    representation: "Quads",
-                };
+				let LDESClient = newEngine();
+				let eventStreamSync = LDESClient.createReadStream(config.url, options);
 
-                let LDESClient = newEngine();
-                let eventStreamSync = LDESClient.createReadStream(
-                    config.url,
-                    options
-                );
+				let data: IMember[] = [];
 
-                let data: IMember[] = [];
+				eventStreamSync.on('data', (member: IMember) => {
+					data.push(member);
+				});
 
-                eventStreamSync.on('data', (member : IMember) => {
-                    data.push(member);
-                });
+				eventStreamSync.on('metadata', (metadata) => {
+					if (metadata.treeMetadata) console.log(metadata.treeMetadata); // follows the structure of the TREE metadata extractor (https://github.com/TREEcg/tree-metadata-extraction#extracted-metadata)
+					console.log(metadata.url); // page from where metadata has been extracted
 
-                eventStreamSync.on('metadata', (metadata) => {
-                    if (metadata.treeMetadata) console.log(metadata.treeMetadata); // follows the structure of the TREE metadata extractor (https://github.com/TREEcg/tree-metadata-extraction#extracted-metadata)
-                    console.log(metadata.url); // page from where metadata has been extracted
-                    
-                    console.log(metadata);
-                    //console.log(metadata.treeMetadata.collections.values().next().value.member);
-                });
+					console.log(metadata);
+					//console.log(metadata.treeMetadata.collections.values().next().value.member);
+				});
 
-                eventStreamSync.on('end', () => {
-                    console.log("No more data!");
-                    resolve(data);
-                });
-            } catch (e) {
-                console.error(e);
-                return reject(e);
-            }
-        });
-    }
-
+				eventStreamSync.on('end', () => {
+					console.log('No more data!');
+					resolve(data);
+				});
+			} catch (e) {
+				console.error(e);
+				return reject(e);
+			}
+		});
+	}
 }
 
 export default LDESClientDatasource;
