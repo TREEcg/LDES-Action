97563da392cf3c6d1b9f38dd65074fc173a4e078
Kasper Zutterman <kasperzutterman1999@gmail.com>
2021-10-01 18:01:03 +0200

    lint: lint

--- src/types/Metadata.ts
+++ src/types/Metadata.ts
@@ -16,84 +16,84 @@ interface TreeMetadata {
 This is a copy from https://github.com/TREEcg/tree-metadata-extraction/blob/master/src/util/Util.ts
 */
 interface Collection {
-	"@context"?: string | object,
-	"@id": string,
-	"@type"?: string[],
-	"view"?: URI[],
-	"subset"?: URI[],
-	"member"?: Member[],
-	"shape"?: Shape[],
-	"totalItems"?: Literal[],
-	"import"?: URI[],
-	"importStream"?: URI[],
-	"conditionalImport"?: ConditionalImport[],
+	'@context'?: string | object;
+	'@id': string;
+	'@type'?: string[];
+	view?: URI[];
+	subset?: URI[];
+	member?: Member[];
+	shape?: Shape[];
+	totalItems?: Literal[];
+	import?: URI[];
+	importStream?: URI[];
+	conditionalImport?: ConditionalImport[];
 	[property: string]: any;
 }
 
 interface Node {
-	"@context"?: string | object,
-	"@id": string,
-	"@type"?: string[],
-	"relation"?: URI[], // Note hydra:next / as:next links are added as Relations of type tree:relation with the target node as the target of the next relation
-	"search"?: IriTemplate[],
-	"retentionPolicy"?: RetentionPolicy[],
-	"import"?: URI[],
-	"importStream"?: URI[],
-	"conditionalImport"?: ConditionalImport[],
+	'@context'?: string | object;
+	'@id': string;
+	'@type'?: string[];
+	relation?: URI[]; // Note hydra:next / as:next links are added as Relations of type tree:relation with the target node as the target of the next relation
+	search?: IriTemplate[];
+	retentionPolicy?: RetentionPolicy[];
+	import?: URI[];
+	importStream?: URI[];
+	conditionalImport?: ConditionalImport[];
 	[property: string]: any;
 }
 
 interface Relation {
-	"@context": any,
-	"@id": string,
-	"@type"?: string[],
-	"remainingItems"?: Literal[],
-	"path"?: any[],
-	"value"?: any[],
-	"node"?: URI[],
-	"import"?: URI[],
-	"importStream"?: URI[],
-	"conditionalImport"?: ConditionalImport[],
+	'@context': any;
+	'@id': string;
+	'@type'?: string[];
+	remainingItems?: Literal[];
+	path?: any[];
+	value?: any[];
+	node?: URI[];
+	import?: URI[];
+	importStream?: URI[];
+	conditionalImport?: ConditionalImport[];
 }
 
 interface Member {
-	"@id"?: string,
+	'@id'?: string;
 	[property: string]: any;
 }
 
 interface Shape {
-	"@id"?: string,
+	'@id'?: string;
 	[property: string]: any;
 }
 
 interface IriTemplate {
-	"@id"?: string,
+	'@id'?: string;
 	[property: string]: any;
 }
 interface ConditionalImport {
-	"@id"?: string,
-	"import"?: any[],
-	"importStream"?: any[],
-	"conditionalImport"?: any[],
+	'@id'?: string;
+	import?: any[];
+	importStream?: any[];
+	conditionalImport?: any[];
 }
 
 interface RetentionPolicy {
-	"@id"?: string,
-	"@type"?: string[],
-	"amount"?: Literal[],
-	"versionKey"?: string[],
-	"path"?: any[],
-	"value"?: any[],
+	'@id'?: string;
+	'@type'?: string[];
+	amount?: Literal[];
+	versionKey?: string[];
+	path?: any[];
+	value?: any[];
 }
 
 interface Literal {
-	"@value": string,
-	"@type"?: string,
-	"@language"?: string,
+	'@value': string;
+	'@type'?: string;
+	'@language'?: string;
 }
 
 interface URI {
-	"@id": string,
+	'@id': string;
 }
 
 export default Metadata;
